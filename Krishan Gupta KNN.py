c48a4ac78cfab2068aff94302e71fdea6724f6c5
krishangupta33 <k.gupta035@gmail.com>
2023-09-26 21:33:27 -0400

    model

--- Krishan Gupta KNN.py
+++ Krishan Gupta KNN.py
@@ -9,7 +9,26 @@ x1=[1,0,-1]
 x2=[1,0,-1]
 
 df=pd.DataFrame({'x1':x1,'x2':x2,'y':y})
+print(df)
 
-print(y)
+#Task 3
+#K-NN model using k=2
+from sklearn.neighbors import KNeighborsClassifier
+knn=KNeighborsClassifier(n_neighbors=2)
+knn.fit(df[['x1','x2']],df['y'])
 
-print('hello world')
+#Task 4 
+#Predicting the y value for x1=0.1 and x2=0.1
+print(knn.predict([[0.1,0.1]]))
+
+
+#Task 5
+#Predicting using predict_proba method and finding probability of being black and blue
+print(knn.predict_proba([[0.1,0.1]]))
+
+#Task 6
+#K-NN model with more parameters
+knn=KNeighborsClassifier(n_neighbors=2,metric='euclidean')
+knn=KNeighborsClassifier(n_neighbors=2,metric='manhattan')
+knn=KNeighborsClassifier(n_neighbors=2,weights='distance')
+
