19b86b4b081eee58ddf04e0b24dc3fd86d94bff1
krishangupta33 <k.gupta035@gmail.com>
2023-09-26 21:14:25 -0400

    libraries

---
+++ Krishan Gupta KNN.py
@@ -0,0 +1,15 @@
+#KNN
+
+#Task 2
+#Create given dataset's dataframe using arrays
+import pandas as pd
+
+y=['black','blue','blue']
+x1=[1,0,-1]
+x2=[1,0,-1]
+
+df=pd.DataFrame({'x1':x1,'x2':x2,'y':y})
+
+print(y)
+
+print('hello world')
